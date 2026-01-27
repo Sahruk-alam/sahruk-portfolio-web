5c2a2f3acb2fcbf96e75f1db1a52ca2812dab291
Sahruk alam sarder <sahrukalam8059@gmail.com>
2026-01-28 02:12:31 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-2xl font-bold text-center items-center'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Routes/Roots.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layout/HomeLayout";
+import Home from "../pages/Home";
+
+
+const router= createBrowserRouter([
+    {
+        path:'/',
+        element:<HomeLayout></HomeLayout>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+}
+      
+   
+    ]);
+    export default router;
